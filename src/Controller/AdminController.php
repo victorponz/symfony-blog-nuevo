bcc371a6b09eca15d54ab57940365c83c9ed3c8d
victorponz <victorponz@gmail.com>
2022-11-09 20:45:52 +0100

    v4.4.2

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -1,18 +1,39 @@
 <?php
 
 namespace App\Controller;
-
+use App\Entity\Category;
+use App\Form\CategoryFormType;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminController extends AbstractController
 {
-    /**
-     * @Route("/admin/images", name="app_images")
-     */
+
+    #[Route('/admin/images', name: 'app_images')]
     public function images(): Response
     {
         return $this->render('admin/images.html.twig', []);
     }
+    #[Route('/admin/categories', name: 'app_categories')]
+    public function categories(ManagerRegistry $doctrine, Request $request): Response
+    {
+        $category = new Category();
+        $form = $this->createForm(CategoryFormType::class, $category);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $category = $form->getData();    
+            $entityManager = $doctrine->getManager();    
+            $entityManager->persist($category);
+            $entityManager->flush();
+        }
+        return $this->render('admin/categories.html.twig', array(
+            'form' => $form->createView() 
+        ));
+    
+    }
+    
+
 }
--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -3,11 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Doctrine\Persistence\ManagerRegistry;
-use App\Entity\Contact;
-use App\Form\ContactFormType;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PageController extends AbstractController
@@ -23,21 +19,9 @@ class PageController extends AbstractController
         return $this->render('page/about.html.twig', []);
     }
     #[Route('/contact', name: 'contact')]
-    public function contact(ManagerRegistry $doctrine, Request $request): Response
+    public function contact(): Response
     {
-        $contact = new Contact();
-        $form = $this->createForm(ContactFormType::class, $contact);
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $contacto = $form->getData();    
-            $entityManager = $doctrine->getManager();    
-            $entityManager->persist($contacto);
-            $entityManager->flush();
-            return $this->redirectToRoute('index', []);
-        }
-        return $this->render('page/contact.html.twig', array(
-            'form' => $form->createView()    
-        ));
+        return $this->render('page/contact.html.twig', []);
     }
     
 }
